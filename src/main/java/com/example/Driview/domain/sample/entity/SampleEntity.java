c089e823bc532fd9383dffc74295370d263fce66
Hyunsoo Song <gustn5522@gmail.com>
2026-02-11 17:15:21 +0900

    feat : 샘플 로직 구현#1

--- src/main/java/com/example/Driview/domain/sample/entity/SampleEntity.java
+++ src/main/java/com/example/Driview/domain/sample/entity/SampleEntity.java
@@ -1,4 +1,35 @@
 package com.example.Driview.domain.sample.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@Entity
+@NoArgsConstructor
+@Table(name = "sample")
 public class SampleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 제목
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    // 내용
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    // 값 변경용 메서드 (setter 대신)
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
