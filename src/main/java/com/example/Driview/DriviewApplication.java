2414abe692abb549b7eab1e944fbc28341747d37
Hyunsoo Song <gustn5522@gmail.com>
2026-02-11 15:33:47 +0900

    init : 프로젝트 초기 셋팅

---
+++ src/main/java/com/example/Driview/DriviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.Driview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Driview/DriviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Driview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
