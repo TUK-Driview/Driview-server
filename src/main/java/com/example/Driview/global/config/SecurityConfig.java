aaea4b3dd652c93ec945000d4a60ecacfe01a7ad
Hyunsoo Song <gustn5522@gmail.com>
2026-05-13 16:36:58 +0900

    fix : faceAI securityConfig

--- src/main/java/com/example/Driview/global/config/SecurityConfig.java
+++ src/main/java/com/example/Driview/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.Driview.global.config;
 import com.example.Driview.domain.user.repository.UserRepository;
 import com.example.Driview.global.jwt.JwtProvider;
 import com.example.Driview.global.security.JwtAuthenticationFilter;
+import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,6 +35,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .dispatcherTypeMatchers(DispatcherType.ASYNC).permitAll()
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/swagger-ui/**",
